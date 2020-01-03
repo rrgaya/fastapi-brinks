031a24257d2c3f0a4397aebb9953ae5d50452e73
Ricardo Gaya <rrgaya@gmail.com>
2020-01-03 13:44:03 -0300

    Added CORS in main file

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from todo import router as todo_router
 
+from starlette.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+# CORS
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# END CORS
 
 @app.get("/")
 def index():
